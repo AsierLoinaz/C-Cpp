44a93079ee4d63a5173f21aab2aaaa8bb502cb58
AsierLoinaz <asier.loinaz@opendeusto.es>
2023-03-14 20:55:29 +0100

    Un poco del 15

--- Ejercicios/Ej15/main.c
+++ Ejercicios/Ej15/main.c
@@ -12,20 +12,49 @@ typedef struct
 char MostrarMenu(){
 
     char input[10];
+    char respuesta;
 
     char menu[] = "1. Introducir asignatura\n"
     "2. Mostrar listado\n"
     "3. Calcular media\n"
-    "Pulsar q para salir"; 
+    "Pulsar q para salir\n"; 
 
     printf("%s", menu);
     fgets(input, 10, stdin);
-    return(*input);
+    sscanf(input, "%s", &respuesta);
+    return(respuesta);
 }
 
-Asignatura introducir();
+void introducir(Asignatura asignaturas[], int size){
+    char inputId[5];
+    char inputNota[5];
 
-void mostrarLista();
+    int id;
+    float nota;
+
+    printf("Introduce el id de la asignatura:\n"); 
+    fgets(inputId, 5, stdin);
+    sscanf(inputId, "%d", &id);
+
+    printf("\nIntroduce la nota de la asignatura:\n");
+    fgets(inputNota, 5, stdin);
+    sscanf(inputNota, "%.2f", &nota);
+
+    Asignatura a = {id, nota}; 
+
+    asignaturas[size + 1] = a;
+
+
+}
+/// @brief Imprime la lista de las asignaturas
+/// @param asignaturas 
+/// @param size del array 
+void mostrarLista(Asignatura asignaturas[], int size){
+    printf("Mostrando lista de asignaturas...\n"); 
+    for (int i = 0; i < size; i++){
+        printf("Id: %i, Nota: %f\n", asignaturas[i].id, asignaturas[i].nota); 
+    }
+}
 
 void calcularMedia();
 
@@ -33,19 +62,24 @@ void calcularMedia();
 int main(){
 
     Asignatura asignaturas[5];
+    int numAsignaturas = 0;
 
     char input = MostrarMenu(); // Input del usuario
+
 
     switch (input)
     {
     case '1':
-        introducir();
+        if (numAsignaturas > 5){
+            printf("No se pueden introducir mas asignaturas, ya hay 5.\n"); 
+        } else{
+        introducir(&asignaturas, numAsignaturas);}
         break;
     case '2':
-        mostrarLista();
+        mostrarLista(asignaturas, numAsignaturas);
         break;
     case '3':
-        calcularMedia();
+       // calcularMedia();
         break;
     case 'q':
         break;
