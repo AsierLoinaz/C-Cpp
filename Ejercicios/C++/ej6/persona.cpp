83a3e7567e21e8807e9977d93e5281ff032f5dd9
AsierLoinaz <asier.loinaz@opendeusto.es>
2023-05-09 11:19:05 +0200

    Ejercicio 6

---
+++ Ejercicios/C++/ej6/persona.cpp
@@ -0,0 +1,68 @@
+#include "persona.h"
+#include <string.h>
+
+
+Persona::Persona(){
+    this->edad = 20;
+    this->nombre = new char[20];
+    strcpy(this->nombre, "Gregory");
+}
+
+Persona::Persona(int edad, char* nombre){
+    this->edad = edad;
+    this->nombre = new char[strlen(nombre) + 1];
+    strcpy(this->nombre, nombre);
+}
+
+Persona::Persona(const Persona &persona){
+    this->edad = persona.edad;
+    this->nombre = new char[strlen(persona.nombre) + 1];   
+    strcpy(this->nombre, persona.nombre);
+}
+
+int Persona::getEdad(){
+    return this->edad;
+}
+
+char* Persona::getNombre(){
+    char* nombre;
+    strcpy(nombre, this->nombre);
+    return nombre;
+}
+
+Persona::~Persona(){
+    delete[] this->nombre;
+}
+
+
+Alumno::Alumno(int numAsignaturas, float* notas){
+    this->numAsignaturas = numAsignaturas;
+    this->notas = new float[numAsignaturas];
+    for (int i = 0; i < this->numAsignaturas; i++){
+        this->notas[i] = notas[i];
+    }
+
+
+}
+
+Alumno::Alumno(const Alumno &al) : Persona(al) { // puedo llamar al constructor copia de persona pasandole el alumno 
+    this->numAsignaturas = al.numAsignaturas;
+        this->notas = new float[al.numAsignaturas];
+        for (int i = 0; i < this->numAsignaturas; i++){
+            this->notas[i] = al.notas[i];
+        }
+}
+
+
+Alumno::Alumno(int edad, char* nombre, int numAsignaturas, float* notas) : Persona(edad, nombre) { //* Equivalente al constructor super
+
+    this->numAsignaturas = numAsignaturas;
+        this->notas = new float[numAsignaturas];
+        for (int i = 0; i < this->numAsignaturas; i++){
+            this->notas[i] = notas[i];
+        }
+}
+
+Alumno::~Alumno(){
+    delete[] this->notas;
+}
---
+++ Ejercicios/C++/ej6/persona.h
@@ -0,0 +1,38 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+
+class Persona{
+    protected:
+        int edad;
+        char* nombre;
+
+    public:
+        Persona();
+        Persona(int edad, char* nombre);
+        Persona (const Persona &persona);
+        ~Persona();
+        int getEdad();
+        char* getNombre();
+};
+
+class Alumno : public Persona{
+
+    private: 
+        int numAsignaturas;
+        float* notas;
+    
+    public:
+        Alumno();
+        Alumno(int numAsignaturas, float* notas);
+        Alumno(const Alumno &al);
+        Alumno(int edad, char* nombre, int numAsignaturas, float* notas);
+
+        ~Alumno();
+        int getNumAsignaturas();
+        float* getNotas();
+    
+
+};
+
+#endif
